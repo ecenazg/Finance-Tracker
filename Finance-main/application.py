61a446f4b0ab0a40e87528b07db4c32437debe2b
Ecenaz Güngör <81537174+ecenazg@users.noreply.github.com>
2023-04-30 19:44:56 +0300

    Update application.py

--- Finance-main/application.py
+++ Finance-main/application.py
@@ -16,24 +16,12 @@ app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
-# Ensure templates are auto-reloaded
-# app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Ensure responses aren't cached
-# @app.after_request
-# def after_request(response):
-#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     response.headers["Expires"] = 0
-#     response.headers["Pragma"] = "no-cache"
-#     return response
 
-# Custom filter
+
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
+
 Session(app)
 
 # Configure CS50 Library to use SQLite database
